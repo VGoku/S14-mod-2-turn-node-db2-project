07805201c29804365e08f773d235989a3f448346
David S. <neojohn2312@gmail.com>
2024-11-05 13:14:53 -0600

    01-cars-started

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
 // STRETCH
+const cars = [
+    {
+    vin: "123456789123",
+    make: "toyota",
+    model: "prius",
+    mileage: 215000,
+    title: "clean",
+    transmission: "manual",
+    },
+    {
+    vin: "123456789123",
+    make: "toyota",
+    model: "corolla",
+    mileage: 115000,
+    title: "salvage",
+    },
+    {
+    vin: "123456789123",
+    make: "ford",
+    model: "focus",
+    mileage: 15000,
+    },
+]
+
+// exports.seed = function(knex) {
+// return knex("cars")
+// .truncate().then(() => {
+// return knex("cars").insert(cars);
+// })
+// }
+
+exports.seed = async function (knex) {
+    await knex("cars").truncate()
+    await knex("cars").insert(cars)
+}
