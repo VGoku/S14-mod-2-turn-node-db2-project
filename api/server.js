b8026a59ed5aa4b9dac861b57b58d07b4334646b
David S. <neojohn2312@gmail.com>
2024-11-11 10:13:06 -0600

    postworkingalcars

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ server.use(express.json());
 server.use("/api/cars", carsRouter)
 
 server.use("*", (req, res, next) => {
-    next({status: 404, message: "not found!!"})
+    next({status: 404, message: "not found!!!"})
 })
 
 server.use((err, req, res, next) => { //eslint-disable-line
